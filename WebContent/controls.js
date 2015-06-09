d047728654a28d10ad42bb19ea12f8dcb24f24bf
Kiran D Cunha <krndon@gmail.com>
2015-06-09 23:49:15 +0530

    snooker initial commit

---
+++ WebContent/controls.js
@@ -0,0 +1,10 @@
+
+// todo: fix balance between friction and velocity for more realistic physics
+
+function controls()
+{
+	// initial white ball velocity
+	friction = 0.990;
+	fps = 20;
+	setInterval(animate, 1000 / fps);
+}
